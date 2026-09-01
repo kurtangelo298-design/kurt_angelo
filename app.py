b8a1ce8d6923c0e2feaa2da97cbd87421f30fa5f
kurtangelo298-design <kurtangelo298@gmail.com>
2026-09-01 08:08:53 +0800

    Update app.py

--- app.py
+++ app.py
@@ -608,7 +608,7 @@ def print_barcode(user_id):
     </html>
     """
 
-if _name_ == '_main_':
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=False)
 from flask import Flask, render_template_string, request, jsonify, make_response
 import psycopg2
